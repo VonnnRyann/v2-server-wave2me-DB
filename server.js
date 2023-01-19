d12332ad2688c30dc9e4ff59c47fbb6434a1181f
admin <you@example.com>
2023-01-19 11:27:09 +0100

    version3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 ﻿import express from  'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import mongoose from  'mongoose';
 import loginRoutes from './routes/loginRoutes.js';
 import blogRoutes from './routes/blogRoutes.js';
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 //ROUTES
 app.use('/', loginRoutes);
